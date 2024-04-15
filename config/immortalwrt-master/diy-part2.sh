4835ff8a3d1ca391792229de3456bde054a6c3dd
Papa Gaye <132003569+papagaye744@users.noreply.github.com>
2024-04-15 11:42:42 +0800

    update build script

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -15,8 +15,8 @@ sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7
 sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
 echo "DISTRIB_SOURCECODE='immortalwrt'" >>package/base-files/files/etc/openwrt_release
 
-# Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
-# sed -i 's/192.168.1.1/192.168.31.4/g' package/base-files/files/bin/config_generate
+# Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.68.3）
+sed -i 's/192.168.1.1/192.168.68.3/g' package/base-files/files/bin/config_generate
 #
 # ------------------------------- Main source ends -------------------------------
 
@@ -25,6 +25,28 @@ echo "DISTRIB_SOURCECODE='immortalwrt'" >>package/base-files/files/etc/openwrt_r
 # Add luci-app-amlogic
 rm -rf package/luci-app-amlogic
 git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+
+# Add luci-theme-design
+git clone https://github.com/gngpp/luci-theme-design.git  -b js --depth=1 package/luci-theme-design
+
+# Add luci-app-mosdns
+# remove v2ray-geodata package from feeds (openwrt-22.03 & master)
+rm -rf feeds/packages/net/v2ray-geodata
+
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+# Add luci-app-openclash
+git clone https://github.com/vernesong/OpenClash.git /tmp/OpenClash
+mv /tmp/OpenClash/luci-app-openclash package/luci-app-openclash
+
+# Add luci-app-adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-luci-app-adguardhome
+
+# Add luci-app-passwall2
+git clone https://github.com/xiaorouji/openwrt-passwall2 /tmp/openwrt-passwall2
+mv /tmp/openwrt-passwall2/luci-app-passwall2 package/luci-app-passwall2
+
 #
 # Apply patch
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
